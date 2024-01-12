d42bab2def061d1d239f0254604212c0357b1936
mohamedsalah986 <146967097+mohamedsalah986@users.noreply.github.com>
2024-01-12 13:49:52 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// JavaScript function to open a link in a new tab
+function navigateTo(url) {
+    window.open(url, '_blank');
+  }
+  
+  // JavaScript function to go back to the main page
+  function goBack() {
+    window.location.href = 'index.html';
+  }
+  
